a87d3bac245e7e72dc3f2bb3e3f7d19c3735dd67
Owen Symes <owen.symes@gmail.com>
2021-02-12 07:56:27 +1030

    Dot2 watchface

---
+++ src/Dot2/Watchy_Dot.cpp
@@ -0,0 +1,22 @@
+#include "Watchy_Dot.h"
+#include <Fonts/FreeMonoBold12pt7b.h>
+#include "Seven_Segment10pt7b.h"
+
+WatchyDot::WatchyDot(){} //constructor
+
+void WatchyDot::drawWatchFace(){
+    display.fillScreen(GxEPD_WHITE);
+    display.drawBitmap(0, 0, bg, DISPLAY_WIDTH, DISPLAY_HEIGHT, GxEPD_BLACK);
+    display.setTextColor(GxEPD_BLACK);
+    display.setFont(&FreeMonoBold12pt7b);
+    display.setCursor(10, 50);
+    if(currentTime.Hour < 10){
+        display.print('0');
+    }
+    display.print(currentTime.Hour);
+    display.print(':');
+    if(currentTime.Minute < 10){
+        display.print('0');
+    }    
+    display.print(currentTime.Minute);
+}
